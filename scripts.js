0c86f3f606e1cf186ff02cb8a308e9b3b2169554
Travis Thomas <tthomas027@gmail.com>
2021-06-12 15:09:42 -0700

    change computerPlay to output lowercase move and create playRound function

--- scripts.js
+++ scripts.js
@@ -4,17 +4,24 @@ function computerPlay() {
     move = Math.floor(Math.random() * 3);
     // if move equals 0 return Rock
     if (move === 0) {
-        return 'Rock';
+        return 'rock';
     // if move equals 1 return Paper
     } else if (move === 1) {
-        return 'Paper';
+        return 'paper';
     }
     // otherwise return 'Scissors'
-    return 'Scissors';
+    return 'scissors';
 
 }
 
 // ask user for their move
 // compare computer move vs player move to see if winner or tie
+function playRound(playerSelection, computerSelection) {
+    // make sure playerSelection is lowercase
+    playSelection = playerSelection.toLowerCase();
+    // check if playerSelection equals computerSelection
+    // check if playerSelection beats computerSelection
+    //check if computerSelection bears playerSelection
+}
 // declare outcome of round
 // keep track of wins
