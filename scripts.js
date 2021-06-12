4433a205deeeca7e5812a223205b6ba11469d1ba
Travis Thomas <tthomas027@gmail.com>
2021-06-12 15:22:21 -0700

    Finished implementing playRound function

--- scripts.js
+++ scripts.js
@@ -15,13 +15,34 @@ function computerPlay() {
 }
 
 // ask user for their move
+
 // compare computer move vs player move to see if winner or tie
 function playRound(playerSelection, computerSelection) {
     // make sure playerSelection is lowercase
-    playSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
     // check if playerSelection equals computerSelection
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    }
     // check if playerSelection beats computerSelection
-    //check if computerSelection bears playerSelection
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Win! Rock beats Scissors'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win! Paper beats Rock'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Win! Scissors beats Paper'
+    }
+    // check if computerSelection beats playerSelection
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You Lose! Paper beats Rock'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lose! Paper beats Scissors'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Rock beats Scissors'
+    }
 }
-// declare outcome of round
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection))
 // keep track of wins
